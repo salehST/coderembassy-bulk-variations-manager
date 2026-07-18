e3e50395b606a34080e4800f660917be6f64a094
Hannan <hannannexus@gmail.com>
2026-07-18 11:27:36 +0600

    Delete assets/admin directory

--- assets/admin/dist/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'e3937f5dc4ee2afcb311');
--- assets/admin/src/components/views/Jobs/JobStatusBadge.jsx
+++
@@ -1,8 +0,0 @@
-import Badge from '../../shared/Badge';
-import { getDisplayStatus, getStatusBadge } from './jobUtils';
-
-export default function JobStatusBadge( { job } ) {
-	const display = getDisplayStatus( job );
-	const badge = getStatusBadge( display );
-	return <Badge status={ badge.status } label={ badge.label } />;
-}
